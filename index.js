0d336ace1c7b9210ed48aaeeaba7b11b95a6232a
Chtulu <jannik.renner@Student.reutlingen-university.de>
2018-10-05 12:29:31 +0200

    New and working

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+users = [];
+connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+io.sockets.on('connection', function(socket){
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+    //Disconnect
+    socket.on('disconnection', function(data){
+        users.splice(users.indexOf(socket.username), 1);
+        updateUsers();
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    //Send message
+    socket.on('send message', function(data){
+        io.sockets.emit('new message', {msg:data, user:socket.username});
+    });
+
+    //New User
+    socket.on('new user', function(data, callback){
+        callback(true);
+        socket.username = data;
+        users.push(socket.username);
+        updateUsers();
+    });
+
+    function updateUsers(){
+        io.sockets.emit('get users', users);
+    }
 });
