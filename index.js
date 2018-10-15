c9b26c6cffec97f4411cd5dd49eddc0e413deb47
Chtulu <jannik.renner@Student.reutlingen-university.de>
2018-10-15 21:14:10 +0200

    bug fix user already exists.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//index.js
+
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -10,12 +12,8 @@ app.use(express.static(__dirname + '/'));
 var users = [];
 var connections = [];
 
-server.listen(process.env.PORT || 3000);
-console.log('Server running');
-
-app.get('/upload', function(req, res){
-    res.sendFile(__dirname + '/upload.html');
-});
+server.listen(process.env.PORT || 3000 || 3030);
+console.log('Server running on port %s', server.address().port);
 
 io.sockets.on('connection', function(socket){
     connect(socket);
@@ -32,14 +30,14 @@ io.sockets.on('connection', function(socket){
 
     //New User
     socket.on('new user', function(data, callback){
-        if(users.includes(socket.username)){
+        if(users.includes(data)){
             socket.emit("user already exists", "The current username already exists. Choose another one instead.");
             callback(false);
             return;
         }
 
         socket.username = data;
-        users.push(socket.username);
+        users.push(data);
         updateUsers();
         notifyUserConnected(data);
         callback(data);
@@ -61,11 +59,13 @@ io.sockets.on('connection', function(socket){
         );
     });
 
+    //Add connection to connections array
     function connect(socket){
         connections.push(socket);
         console.log('Connected: %s sockets connected', connections.length);
     }
 
+    //Remove user from users array and remove connection from connections array. Update users.
     function disconnect(socket) {
         users.splice(users.indexOf(socket.username), 1);
         updateUsers();
@@ -74,11 +74,13 @@ io.sockets.on('connection', function(socket){
         console.log('Disconnected: %s sockets connected', connections.length);
     }
 
+    //Update users.
     function updateUsers(){
         console.log("Users: " + users);
         io.sockets.emit('get users', users);
     }
 
+    //Notify that new user connected.
     function notifyUserConnected(username){
         var alert = {};
         alert.msg = "User " + username + " entered the room.";
@@ -86,6 +88,7 @@ io.sockets.on('connection', function(socket){
         io.sockets.emit('user connected', alert);
     }
 
+    //Notify that user disconnected.
     function notifyUserDisconnected(username){
         var alert = {};
         alert.msg = "User: " + username + " left the room.";
@@ -93,20 +96,23 @@ io.sockets.on('connection', function(socket){
         io.sockets.emit('user disconnected', alert);
     }
 
+    //Send a data object to all connected sockets.
     function sendMessageToAllUsers(data, timestamp){
         io.sockets.emit('new message', {type:'all', msg:data.msg, fileData:data.fileData, user:socket.username, timestamp:timestamp});
     }
 
+    //Parse the data object's massage to distinguis between different commands.
     function parseMessage(data, socket){
         var timestamp = createTimestamp();
 
+        //Set a message value if there is no massage to avoid errors.
         if(data.msg == null || data.msg == undefined){
             data.msg = "";
         }
 
-        if(data.msg.startsWith("/list")){
+        if(data.msg.startsWith("/list")){   //Check if the massage is a list command.
             socket.emit('new message list', {users:users});
-        }else if(data.msg.startsWith("/whisper")){
+        }else if(data.msg.startsWith("/whisper")){  //CHeck if the message is a whisper command.
             var res = data.msg.split(":");
             var username = res[1].split(" ", 1);
             var msg = res[1].slice(username[0].length, res[1].length);
@@ -115,11 +121,12 @@ io.sockets.on('connection', function(socket){
                 whisper_socket.emit('new message',  {type:'whisper', msg:msg, fileData:data.fileData, user:socket.username, timestamp:timestamp});
                 socket.emit('new message',  {type:'whisper', msg:msg, fileData:data.fileData, user:socket.username, timestamp:timestamp});
             }
-        }else{
+        }else{  //The message is no specific command. Send it to all users.
             sendMessageToAllUsers(data, timestamp);
         }
     }
 
+    //Create a timestamp object with the current time in hour:minute.
     function createTimestamp(){
         var min = new Date().getMinutes();
         var minutes;
