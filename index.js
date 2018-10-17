dbcbd9852feb87136bcbb6b1eda96a0d6f2c8396
Chtulu <jannik.renner@Student.reutlingen-university.de>
2018-10-17 13:02:55 +0200

    design changes

--- index.js
+++ index.js
@@ -30,16 +30,21 @@ io.sockets.on('connection', function(socket) {
 
     //New User
     socket.on('new user', function (data, callback) {
-        if (users.includes(data)) {
+        socket.username = data;
+        var user = {
+            id:socket.username
+        };
+
+        //Check if user already exists
+        if (userAlreadyExists(user.id)) {
             socket.emit("user already exists", "The current username already exists. Choose another one instead.");
             callback(false);
             return;
         }else {
-            socket.username = data;
-            users.push(data);
+            users.push(user);
+            notifyUserConnected(user.id);
+            callback(user.id);
             updateUsers();
-            notifyUserConnected(data);
-            callback(data);
         }
     });
     //Add connection to connections array
@@ -49,29 +54,39 @@ io.sockets.on('connection', function(socket) {
     }
     //Remove user from users array and remove connection from connections array. Update users.
     function disconnect(socket) {
-        users.splice(users.indexOf(socket.username), 1);
-        updateUsers();
+        var user;
+        users.forEach(function(element){
+            if(element.id == socket.username){
+                user = element;
+            }
+        });
+        if(user != null && user != undefined && user.id != undefined && userAlreadyExists(user.id)){
+            //Disconnect user
+            users.splice(users.indexOf(user), 1);
+            updateUsers();
+        } 
+        //Disconnect connection              
         connections.splice(connections.indexOf(socket), 1);
+        socket.emit('disconnect socket', "");  
         notifyUserDisconnected(socket.username);
         console.log('Disconnected: %s sockets connected', connections.length);
     }
 
     //Update users.
     function updateUsers(){
-        console.log("Users: " + users);
         io.sockets.emit('get users', users);
     }
 
     //Notify that new user connected.
     function notifyUserConnected(username){
-        var msg = "User " + username + " entered the room.";
+        var msg = username + " entered the room.";
         var timestamp = createTimestamp();
         io.sockets.emit('new message',  {type:'connected', msg:msg, fileData:null, user:socket.username, timestamp:timestamp});
     }
 
     //Notify that user disconnected.
     function notifyUserDisconnected(username){
-        var msg = "User: " + username + " left the room.";
+        var msg = username + " left the room.";
         var timestamp = createTimestamp();
         io.sockets.emit('new message',  {type:'disconnected', msg:msg, fileData:null, user:socket.username, timestamp:timestamp});
     }
@@ -83,27 +98,31 @@ io.sockets.on('connection', function(socket) {
 
     //Parse the data object's massage to distinguis between different commands.
     function parseMessage(data, socket){
-        var timestamp = createTimestamp();
 
-        //Set a message value if there is no massage to avoid errors.
-        if(data.msg == null || data.msg == undefined){
-            data.msg = "";
-        }
+        //Check if message or file is attached. If both are missing, don't send
+        if(data.msg != null && data.msg != undefined && data.msg != "" || data.fileData.file != null && data.fileData.file != undefined){
 
-        if(data.msg.startsWith("/list")){   //Check if the massage is a list command.
-            socket.emit('new message', {type:'list', msg:users, fileData:data.fileData, user:socket.username, timestamp:timestamp});
-        }else if(data.msg.startsWith("/whisper")){  //CHeck if the message is a whisper command.
-            var res = data.msg.split(":");
-            var username = res[1].split(" ", 1);
-            var msg = res[1].slice(username[0].length, res[1].length);
-            var whisper_socket = connections.find(socket => socket.username == username);
-            if(whisper_socket){
-                whisper_socket.emit('new message',  {type:'whisper', msg:msg, fileData:data.fileData, user:socket.username, timestamp:timestamp});
-                socket.emit('new message',  {type:'whisper', msg:msg, fileData:data.fileData, user:socket.username, timestamp:timestamp});
+            var timestamp = createTimestamp();
+    
+            if(data.msg.startsWith("/list")){   //Check if the massage is a list command.
+                socket.emit('new message', {type:'list', msg:users, fileData:data.fileData, user:socket.username, timestamp:timestamp});
+            }else if(data.msg.startsWith("/whisper")){  //CHeck if the message is a whisper command.
+                //Split username from message
+                var res = data.msg.split(":");
+                var username = res[1].split(" ", 1);
+                var msg = res[1].slice(username[0].length, res[1].length);
+                //Get socket related to username
+                var whisper_socket = connections.find(socket => socket.username == username);
+                //Send message to self and whisper user
+                if(whisper_socket){
+                    whisper_socket.emit('new message',  {type:'whisper', msg:msg, fileData:data.fileData, user:socket.username, timestamp:timestamp});
+                    socket.emit('new message',  {type:'whisper', msg:msg, fileData:data.fileData, user:socket.username, timestamp:timestamp});
+                }
+            }else{  //The message is no specific command. Send it to all users.
+                sendMessageToAllUsers(data, timestamp);
             }
-        }else{  //The message is no specific command. Send it to all users.
-            sendMessageToAllUsers(data, timestamp);
         }
+        
     }
 
     //Create a timestamp object with the current time in hour:minute.
@@ -116,5 +135,15 @@ io.sockets.on('connection', function(socket) {
             minutes = min;
         }
         return timestamp = new Date().getHours() + ":" + minutes;
+    }
+
+    function userAlreadyExists(userId){
+        var userAlreadyExists = false;
+        users.forEach(function(element) {
+            if(element.id === userId){
+                userAlreadyExists = true;
+            }
+        });
+        return userAlreadyExists;
     }
 });
